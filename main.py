03db29a88f01c965aa08c3bec143932e9e2dff9b
jacob.ancheta <jecepee@gmail.com>
2025-12-03 19:59:27 -0500

    switch to url input

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from selenium.webdriver.chrome.service import Service
 
 service = Service(r"C:\Users\Jacob\OneDrive\Desktop\tools\chromedriver-win64\chromedriver.exe")
 driver = webdriver.Chrome(service=service)
-driver.get("https://www.google.com")
+url = input("paste url here (must be unlisted or): ")
+driver.get(str(url))
+#https://www.google.com
 input("Press Enter to close the browser...")
 driver.quit()
